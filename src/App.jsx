fb398d9ddca5eb496a84cd6cdf0d377ae868b49d
alexmldev <alexmldev@gmail.com>
2023-11-06 15:17:54 +0100

    create row component with vertical default props and horizontal option with flex

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
 
+import Row from "./ui/Row";
 import Heading from "./ui/Heading";
 import Input from "./ui/Input";
 import Button from "./ui/Button";
 
 const StyledApp = styled.main`
-  background-color: orangered;
+  /* background-color: orangered; */
   padding: 20px;
 `;
 
@@ -15,13 +16,23 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <Heading as="h1">The Wild Oasis</Heading>
-        <Heading as="h2">Check in and out</Heading>
-        <Button onClick={() => alert("Check in")}>Check in</Button>
-        <Button onClick={() => alert("Check out")}>Check out</Button>
-        <Heading as="h3">Form</Heading>
-        <Input type="number" placeholder="Number of guests" />
-        <Input type="number" placeholder="Number of guests" />
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The Wild Oasis</Heading>
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button onClick={() => alert("Check in")}>Check in</Button>
+              <Button onClick={() => alert("Check out")}>Check out</Button>
+            </div>
+          </Row>
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guests" />
+              <Input type="number" placeholder="Number of guests" />
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
