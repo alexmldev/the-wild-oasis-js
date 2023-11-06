52916ffb992ce337b8240e97813ff587c3ff57be
alexmldev <alexmldev@gmail.com>
2023-11-06 16:10:44 +0100

    update applayout component to render outlet and newly created header & sidebar components

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,4 +1,17 @@
+import { Outlet } from "react-router-dom";
+import Sidebar from "./Sidebar";
+import Header from "./Header";
+
 function AppLayout() {
-  return <div>AppLayout</div>;
+  return (
+    <div>
+      <Header />
+      <Sidebar />
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
 }
+
 export default AppLayout;
