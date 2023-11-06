d0cf98f3da8dbd1164d8da9a941a8fd734ebe6aa
alexmldev <alexmldev@gmail.com>
2023-11-06 16:31:09 +0100

    add styling to MainNav component and add NavLinks

--- src/ui/MainNav.jsx
+++ src/ui/MainNav.jsx
@@ -1,3 +1,5 @@
+import { NavLink } from "react-router-dom";
+
 import styled from "styled-components";
 
 const NavList = styled.ul`
@@ -6,7 +8,7 @@ const NavList = styled.ul`
   gap: 0.8rem;
 `;
 
-const Link = styled.a`
+const StyledNavLink = styled(NavLink)`
   &:link,
   &:visited {
     display: flex;
@@ -44,3 +46,19 @@ const Link = styled.a`
     color: var(--color-brand-600);
   }
 `;
+
+function MainNav() {
+  return (
+    <nav>
+      <NavList>
+        <li>
+          <StyledNavLink to="/dashboard">Home</StyledNavLink>
+        </li>
+        <li>
+          <StyledNavLink to="/bookings">Bookings</StyledNavLink>
+        </li>
+      </NavList>
+    </nav>
+  );
+}
+export default MainNav;
