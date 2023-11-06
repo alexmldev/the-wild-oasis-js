a5c48a126a670a33496f01a646124d770f84a5b2
alexmldev <alexmldev@gmail.com>
2023-11-06 15:53:38 +0100

    clear App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,4 @@
-import styled from "styled-components";
-import GlobalStyles from "./styles/GlobalStyles";
-
-import Row from "./ui/Row";
-import Heading from "./ui/Heading";
-import Input from "./ui/Input";
-import Button from "./ui/Button";
-
-const StyledApp = styled.main`
-  /* background-color: orangered; */
-  padding: 20px;
-`;
-
 function App() {
-  return (
-    <>
-      <GlobalStyles />
-      <StyledApp>
-        <Row>
-          <Row type="horizontal">
-            <Heading as="h1">The Wild Oasis</Heading>
-            <div>
-              <Heading as="h2">Check in and out</Heading>
-              <Button onClick={() => alert("Check in")}>Check in</Button>
-              <Button
-                variation="secondary"
-                size="small"
-                onClick={() => alert("Check out")}
-              >
-                Check out
-              </Button>
-            </div>
-          </Row>
-          <Row>
-            <Heading as="h3">Form</Heading>
-            <form>
-              <Input type="number" placeholder="Number of guests" />
-              <Input type="number" placeholder="Number of guests" />
-            </form>
-          </Row>
-        </Row>
-      </StyledApp>
-    </>
-  );
+  return <div>App</div>;
 }
-
 export default App;
