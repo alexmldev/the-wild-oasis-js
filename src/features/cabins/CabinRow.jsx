5d964a1c3fae986f65eb99a5150c5ca095811528
alexmldev <alexmldev@gmail.com>
2023-11-07 16:42:46 +0100

    add basic error handling & add basic alerts to success-error handling in delete useMutation

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -59,10 +59,12 @@ function CabinRow({ cabin }) {
   const { isLoading: isDeleting, mutate } = useMutation({
     mutationFn: deleteCabin,
     onSuccess: () => {
+      alert("Cabin successfully deleted");
       queryClient.invalidateQueries({
         queryKey: ["cabins"],
       });
     },
+    onError: err => alert(err.message),
   });
 
   return (
