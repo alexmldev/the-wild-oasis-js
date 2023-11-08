99f754d4b6b8ac050757f3fb97877ea10abf6811
alexmldev <alexmldev@gmail.com>
2023-11-08 14:31:47 +0100

    update createCabin Api to support file upload & change RLS options in Supabase cabin-image bucket

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://qqigocdjjulnplhyngtb.supabase.co";
+export const supabaseUrl = "https://qqigocdjjulnplhyngtb.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFxaWdvY2RqanVsbnBsaHluZ3RiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTkyOTI4ODEsImV4cCI6MjAxNDg2ODg4MX0.1OlUSNo09ROtHbfPQ3rmVKob2Idtq5dyXds3KM23a40";
 const supabase = createClient(supabaseUrl, supabaseKey);
