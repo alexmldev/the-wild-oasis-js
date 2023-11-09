cb90103462aa9315a1d172ce3f1cb3eabc55cdc6
alexmldev <alexmldev@gmail.com>
2023-11-10 00:28:33 +0100

    render modal with createPortal in the body of the document

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,3 +1,5 @@
+import { createPortal } from "react-dom";
+
 import styled from "styled-components";
 import { HiXMark } from "react-icons/hi2";
 
@@ -51,7 +53,7 @@ const Button = styled.button`
 `;
 
 function Modal({ children, onClose }) {
-  return (
+  return createPortal(
     <Overlay>
       <StyledModal>
         <Button onClick={onClose}>
@@ -59,7 +61,8 @@ function Modal({ children, onClose }) {
         </Button>
         <div>{children}</div>
       </StyledModal>
-    </Overlay>
+    </Overlay>,
+    document.body
   );
 }
 
