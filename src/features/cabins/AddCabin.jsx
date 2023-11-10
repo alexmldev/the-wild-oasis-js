db5f9768276c608c22ba3522f454ff94a6dd5258
alexmldev <alexmldev@gmail.com>
2023-11-10 01:27:56 +0100

    add back in show-table modal into addcabin component

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,8 +1,7 @@
-import { useState } from "react";
-
 import CreateCabinForm from "./CreateCabinForm";
 import Button from "../../ui/Button";
 import Modal from "../../ui/Modal";
+import CabinTable from "./CabinTable";
 
 function AddCabin() {
   return (
@@ -14,31 +13,14 @@ function AddCabin() {
         <CreateCabinForm />
       </Modal.Window>
 
-      {/* <Modal.Open opens='table'>
-      <Button>Show table</Button>
-    </Modal.Open>
-    <Modal.Winow name='table'>
-      <CreateCabinForm />
-    </Modal.Winow> */}
+      <Modal.Open opens="table">
+        <Button>Show table</Button>
+      </Modal.Open>
+      <Modal.Window name="table">
+        <CabinTable />
+      </Modal.Window>
     </Modal>
   );
 }
-
-// function AddCabin() {
-//   const [isOpenModal, setIsOpenModal] = useState(false);
-
-//   return (
-//     <div>
-//       <Button onClick={() => setIsOpenModal(show => !show)}>
-//         Add new cabin
-//       </Button>
-//       {isOpenModal && (
-//         <Modal onClose={() => setIsOpenModal(false)}>
-//           <CreateCabinForm onCloseModal={() => setIsOpenModal(false)} />
-//         </Modal>
-//       )}
-//     </div>
-//   );
-// }
 
 export default AddCabin;
