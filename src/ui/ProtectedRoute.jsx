354af44028f39759108141f4601cfedf929b7630
alexmldev <alexmldev@gmail.com>
2023-11-22 23:23:19 +0100

    implement loading spinner while logging in inside the protectedRoute component with the useUser hook

--- src/ui/ProtectedRoute.jsx
+++ src/ui/ProtectedRoute.jsx
@@ -1,7 +1,29 @@
+import { useUser } from "../features/authentication/useUser";
+
+import styled from "styled-components";
+
+import Spinner from "./Spinner";
+
+const Fullpage = styled.div`
+  height: 100vh;
+  background-color: var(--color-grey-50);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
 function ProtectedRoute({ children }) {
   // 1. Load the authenticated user
+  const { user, isLoading } = useUser();
 
   // 2. While loading, show a spinner
+  if (isLoading)
+    return (
+      <Fullpage>
+        <Spinner />
+      </Fullpage>
+    );
+
   // 3. If there is NO authenticated user, redirect to the /login
   // 4. If there IS a user, render the app
   return children;
