195bb5817e8e1371956a28967dbed84e9614ad8d
alexmldev <alexmldev@gmail.com>
2023-11-22 23:35:44 +0100

    leverage useQueryClient in useLogin component to manually set query data to fetch user again for when the user comes back after a while

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,4 +1,4 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
 
 import { login as loginApi } from "../../services/apiAuth";
@@ -6,6 +6,7 @@ import { login as loginApi } from "../../services/apiAuth";
 import { toast } from "react-hot-toast";
 
 export function useLogin() {
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
 
   const { mutate: login, isLoading } = useMutation({
@@ -15,6 +16,7 @@ export function useLogin() {
         password,
       }),
     onSuccess: user => {
+      queryClient.setQueriesData(["user"], user);
       navigate("/dashboard");
     },
     onError: err => {
