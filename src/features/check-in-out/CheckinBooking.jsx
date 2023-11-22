15a2184445dc0467ff2946ac36a4d23dedc9e88b
alexmldev <alexmldev@gmail.com>
2023-11-22 10:44:06 +0100

    implement complete optional breakfast feature

--- src/features/check-in-out/CheckinBooking.jsx
+++ src/features/check-in-out/CheckinBooking.jsx
@@ -56,8 +56,16 @@ function CheckinBooking() {
     if (!confirmPaid) return;
 
     if (addBreakfast) {
+      checkin({
+        bookingId,
+        breakfast: {
+          hasBreakfast: true,
+          extrasPrice: optionalBreakfastPrice,
+          totalPrice: totalPrice + optionalBreakfastPrice,
+        },
+      });
     } else {
-      checkin(bookingId);
+      checkin({ bookingId, breakfast: {} });
     }
   }
 
