16b67580d17dd97c373229b9692398f3120300d4
alexmldev <alexmldev@gmail.com>
2023-11-23 17:58:26 +0100

    update Dashboard page to include DashboardLayout component

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,12 +1,16 @@
-import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import Heading from "../ui/Heading";
+import DashboardLayout from "../features/dashboard/DashboardLayout";
 
 function Dashboard() {
   return (
-    <Row type="horizontal">
-      <Heading as="h1">Dashboard</Heading>
-      <p>TEST</p>
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">Dashboard</Heading>
+        <p>TEST</p>
+      </Row>
+      <DashboardLayout />
+    </>
   );
 }
 
