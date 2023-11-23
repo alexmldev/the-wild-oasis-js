6a2960e39bb2fa6165470cf25a52ae1592f85999
alexmldev <alexmldev@gmail.com>
2023-11-23 17:45:01 +0100

    update DarkModeToggle component to conditional render icon

--- src/ui/DarkModeToggle.jsx
+++ src/ui/DarkModeToggle.jsx
@@ -1,14 +1,17 @@
-import { HiOutlineMoon } from "react-icons/hi2";
-import ButtonIcon from "./ButtonIcon";
 import { useDarkMode } from "../context/DarkModeContext";
+
+import { HiOutlineMoon, HiOutlineSun } from "react-icons/hi2";
+
+import ButtonIcon from "./ButtonIcon";
 
 function DarkModeToggle() {
   const { isDarkMode, toggleDarkMode } = useDarkMode();
 
   return (
-    <ButtonIcon>
-      <HiOutlineMoon />
+    <ButtonIcon onClick={toggleDarkMode}>
+      {isDarkMode ? <HiOutlineSun /> : <HiOutlineMoon />}
     </ButtonIcon>
   );
 }
+
 export default DarkModeToggle;
