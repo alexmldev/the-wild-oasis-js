d68c207cb7324707514395c3f54c455ecb510799
alexmldev <alexmldev@gmail.com>
2023-11-24 11:12:21 +0100

    wrap App with ErrorBoundary component from react-error-boundary library

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+import { ErrorBoundary } from "react-error-boundary";
+
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>
 );
