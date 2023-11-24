474568e92f71e62f1c52700452d536b49a3d6776
alexmldev <alexmldev@gmail.com>
2023-11-24 11:20:28 +0100

    add onReset prop to the ErrorBoundary Component in Main

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,10 @@ import ErrorFallback from "./ui/ErrorFallback.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ErrorBoundary FallbackComponent={ErrorFallback}>
+    <ErrorBoundary
+      FallbackComponent={ErrorFallback}
+      onReset={() => window.location.replace("/")}
+    >
       <App />
     </ErrorBoundary>
   </React.StrictMode>
